d9bef32b5acd4a9c86d2d2735dbdf4d2bf2dbe5e
hkal <_@hkal.me>
2014-05-01 11:04:59 -0400

    update bower recipe

---
+++ bower/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:npm][:version] = "1.4.7"
+default[:nodejs][:install_method] = 'package'
--- bower/metadata.rb
+++ bower/metadata.rb
@@ -1,7 +1,8 @@
 name             'bower'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Cloudspace, LLC'
+maintainer_email ''
+license          'MIT'
 description      'Installs/Configures bower'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
+depends          "npm"
