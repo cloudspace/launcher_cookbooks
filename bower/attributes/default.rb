b77b52992723b1a75dc84091d93bc3c2f0b9d19e
hkal <_@hkal.me>
2014-05-02 11:26:42 -0400

    no longer need these

--- bower/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default[:npm][:version] = "1.4.7"
-default[:nodejs][:install_method] = 'package'
--- bower/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'bower'
-maintainer       'Cloudspace, LLC'
-maintainer_email ''
-license          'MIT'
-description      'Installs/Configures bower'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
-depends          "npm"
